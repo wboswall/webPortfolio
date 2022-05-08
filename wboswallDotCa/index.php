01ce9ae1f034e5d63bc6fc94db41a6afc8404dd3
William Boswall <wboswall@live.com>
2022-05-07 23:38:08 -0300

    Update project
    updated the codebase with php code.

--- wboswallDotCa/index.php
+++ wboswallDotCa/index.php
@@ -3,7 +3,7 @@
 		<head>
 		<title>Home</title>
 		<meta charset="utf-8">
-		<meta name="description" content="Portfolio">
+		<meta name="description" content="Personal Website">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="keywords" content="HTML, CSS, JavaScript, React, Angular, Vue, PHP, MySQL, Blog, William, Boswall, GitHub">
 		<meta name="author" content="William Boswall">
@@ -14,15 +14,8 @@
 		<link script src="avonlea.js"async></script>
 	</head>
 	<body>
-				<header class="Jumbotron">
-					<menu>
-						<li><a href="index.php" target="_blank">Home</a></li>
-						<li><a href="https://github.com/wboswall" target="_blank">My Github Repository</a></li>
-						<li><a href="blog.php" target="_blank">Blog</a></li>
-						<li><a href="portfolio.php" target="_blank">My Portfolio</a></li>
-						<li><a href="contact.php" target="_blank">Contact</a></li>
-					</menu>
-				</header>
+		<?php require ("phpScripts/header.php");?>
+				
 				<section class="Intro">
 					<h1>Welcome!</h1>
 					<p> Thank you for visiting my website!  </p>
@@ -32,11 +25,6 @@
         <section>
           <h1> Page under construction!</h1>
         </section>
-				<footer class="ListFooter">
-					<p> A special thanks to <a href="https://pixabay.com" target="_blank">Pixabay</a> for providing free images.  Much appreciated. </p>
-					<p> Thanks to <a href="https://realfavicongenerator.net/svg-favicon/" target="_blank">Favicon generator</a> for generating favicons for my website.</p>
-					<p class="email"> For email inquiries: please see <a href="contact.php" target="_blank">Contact Page</a></p>
-          <p> Copyright &copy; 2022 William Boswall. All rights reserved.</p>
-				</footer>
+				<?php require("phpScrpts/footer.php");?>
 			</body>
 	</html> 
--- wboswallDotCa/phpScripts/footer.php
+++ wboswallDotCa/phpScripts/footer.php
@@ -1,3 +1,8 @@
 <?php
-
+echo('<footer class="ListFooter">
+<p> A special thanks to <a href="https://pixabay.com" target="_blank">Pixabay</a> for providing free images.  Much appreciated. </p>
+<p> Thanks to <a href="https://realfavicongenerator.net/svg-favicon/" target="_blank">Favicon generator</a> for generating favicons for my website.</p>
+<p class="email"> For email inquiries: please see <a href="contact.php" target="_blank">Contact Page</a></p>
+<p> Copyright &copy; 2022 William Boswall. All rights reserved.</p>
+</footer>');
 ?>
